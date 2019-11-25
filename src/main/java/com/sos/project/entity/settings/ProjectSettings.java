f9f7c4879e3fc8964f9f885c24e79dd8d2ec4aad
Louis Weyrich <louisweyrich.aws@gmail.com>
2019-11-24 19:56:23 -0800

    entity changes

--- src/main/java/com/sos/project/entity/settings/ProjectSettings.java
+++ src/main/java/com/sos/project/entity/settings/ProjectSettings.java
@@ -1,11 +1,30 @@
 package com.sos.project.entity.settings;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "project_settings", schema = "project_scheduler")
+@Entity(name="ProjectSettings")
 public class ProjectSettings {
 	
+	@Id
+	@Column(name="project_id", nullable = false, updatable = false)
 	private Long projectId;
+	
+	@Column(name="resource_folder", nullable = false, updatable = false)
 	private String resourcesFolder;
-	private Theme style;
+	
+	@ManyToOne(targetEntity = Theme.class, optional = false,fetch = FetchType.EAGER)
+	@JoinColumn(name = "theme_id")
+	private Theme theme;
 
+	
+	
 	public ProjectSettings() {
 		// TODO Auto-generated constructor stub
 	}
@@ -26,12 +45,12 @@ public class ProjectSettings {
 		this.resourcesFolder = resourcesFolder;
 	}
 
-	public Theme getStyle() {
-		return style;
+	public Theme getTheme() {
+		return theme;
 	}
 
-	public void setStyle(Theme style) {
-		this.style = style;
+	public void setTheme(Theme theme) {
+		this.theme = theme;
 	}
 
 }
--- src/main/java/com/sos/project/entity/settings/Theme.java
+++ src/main/java/com/sos/project/entity/settings/Theme.java
@@ -2,14 +2,29 @@ package com.sos.project.entity.settings;
 
 import java.util.Set;
 
-public class Theme {
-	
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+public class Theme 
+{
+	@Id
+	@Column(name="setting_id", columnDefinition = "INT")
 	private Long settingId;
+	
+	@Column(name="name", insertable = true, updatable = false, length = 26, columnDefinition = "VARCHAR(26)")
 	private String name;
+	
+	@ManyToMany(targetEntity = Style.class, fetch = FetchType.EAGER)
+	@JoinTable(name = "theme_style", joinColumns = @JoinColumn(name = "style_id"), inverseJoinColumns = @JoinColumn(name = "theme_id"))
 	private Set <Style> styles;
 
-	public Theme() {
-		// TODO Auto-generated constructor stub
+	public Theme() 
+	{
+		// Do Nothing
 	}
 
 	public Long getSettingId() {
